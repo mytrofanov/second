a69a1675bdca80648a15a5d7fcd955a4e424bf1a
mytrofanov <mytrofanovmaxym@gmail.com>
2021-04-02 12:06:05 +0300

    Упаковали все в store state, ООП, рефакторинг. потеря this, привязка при помощи bind

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,79 +1,82 @@
-let rerenderEntireTree =() => {
-    console.log('State changed');
-}
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'First post', count: 5, discount: 0},
+                {id: 2, message: 'Second post', count: 7, discount: 1},
+                {id: 3, message: 'Third post', count: 8, discount: 10},
+                {id: 4, message: 'Forth post', count: 9, discount: 4},
+                {id: 5, message: 'Fifth post', count: 10, discount: 2},
+                {id: 6, message: 'Six post', count: 11, discount: 3},
+                {id: 7, message: 'Seven post', count: 14, discount: 4},
+                {id: 8, message: 'Eight post', count: 15, discount: 2},
+                {id: 9, message: 'Nine post', count: 7, discount: 2}
+            ],
+            newPostText: 'it-kamasutra'
+        },
 
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'First post', count: 5, discount: 0},
-            {id: 2, message: 'Second post', count: 7, discount: 1},
-            {id: 3, message: 'Third post', count: 8, discount: 10},
-            {id: 4, message: 'Forth post', count: 9, discount: 4},
-            {id: 5, message: 'Fifth post', count: 10, discount: 2},
-            {id: 6, message: 'Six post', count: 11, discount: 3},
-            {id: 7, message: 'Seven post', count: 14, discount: 4},
-            {id: 8, message: 'Eight post', count: 15, discount: 2},
-            {id: 9, message: 'Nine post', count: 7, discount: 2}
-        ],
-        newPostText: 'it-kamasutra'
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: 'Sasha', ava: 'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg'},
+                {id: 2, name: 'Kolya', ava: 'https://klike.net/uploads/posts/2019-03/1551511801_1.jpg'},
+                {id: 3, name: 'Petya', ava: 'https://www.meme-arsenal.com/memes/cbfd4797382778baf1f41b8439399262.jpg'},
+                {
+                    id: 4,
+                    name: 'Vasya',
+                    ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnIZ65bUZfqwc204EF5GN3tpbfY5zQ2WiB1A&usqp=CAU'
+                },
+                {
+                    id: 5,
+                    name: 'Nino',
+                    ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbMxEyi1yeivrdaCitJxEftqshPFPEouuz2A&usqp=CAU'
+                },
+                {
+                    id: 6,
+                    name: 'Dina',
+                    ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa8SCQKFAMJ8BW_2FXCdUyTXm66A4eksY2iA&usqp=CAU'
+                },
+                {
+                    id: 7,
+                    name: 'Pablo',
+                    ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYNPFMEDOAjlZxz1E1bSrNI-PlOq2m1tRh0g&usqp=CAU'
+                }
+            ],
+            messages: [
+                {id: 1, message: 'Hi!'},
+                {id: 2, message: 'How are you doing?'},
+                {id: 3, message: 'Whats up?'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'},
+            ]
+        }
+    },
+    getState () {
+      return this._state;
+    },
+    _callSubscriber () {
+        console.log('State changed');
+    },
+    addPost () {
+        let newPost = {
+            id: 10,
+            message: this._state.profilePage.newPostText,
+            count: 0, discount: 0
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer; //наблюдатель
     },
 
-    messagesPage: {
-        dialogs: [
-            {id: 1, name: 'Sasha', ava: 'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg'},
-            {id: 2, name: 'Kolya', ava: 'https://klike.net/uploads/posts/2019-03/1551511801_1.jpg'},
-            {id: 3, name: 'Petya', ava: 'https://www.meme-arsenal.com/memes/cbfd4797382778baf1f41b8439399262.jpg'},
-            {
-                id: 4,
-                name: 'Vasya',
-                ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnIZ65bUZfqwc204EF5GN3tpbfY5zQ2WiB1A&usqp=CAU'
-            },
-            {
-                id: 5,
-                name: 'Nino',
-                ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbMxEyi1yeivrdaCitJxEftqshPFPEouuz2A&usqp=CAU'
-            },
-            {
-                id: 6,
-                name: 'Dina',
-                ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa8SCQKFAMJ8BW_2FXCdUyTXm66A4eksY2iA&usqp=CAU'
-            },
-            {
-                id: 7,
-                name: 'Pablo',
-                ava: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYNPFMEDOAjlZxz1E1bSrNI-PlOq2m1tRh0g&usqp=CAU'
-            }
-        ],
-        messages: [
-            {id: 1, message: 'Hi!'},
-            {id: 2, message: 'How are you doing?'},
-            {id: 3, message: 'Whats up?'},
-            {id: 4, message: 'Yo'},
-            {id: 5, message: 'Yo'},
-        ]
-    }
 }
 
-window.state = state;
 
-export const addPost = () => {
+export default store;
 
-    let newPost = {
-        id: 10,
-        message: state.profilePage.newPostText,
-        count: 0, discount: 0
-    }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; //наблюдатель
-}
-export default state;
+window.store = store;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import state, {subscribe} from "./Redux/state";
+import store from "./Redux/state";
 import reportWebVitals from "./reportWebVitals";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, updateNewPostText} from "./Redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state} addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root'));
 }
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
