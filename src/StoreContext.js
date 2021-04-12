5b8f24a860a36fc300affdf9124c21304e03ad47
mytrofanov <mytrofanovmaxym@gmail.com>
2021-04-12 11:49:45 +0300

    44й урок  Контекст избавляемся от пропсов контекст забрасываем в контейнерную компоненту презентационная компонента по прежнему обращается к контейнерной + рефактор файла index.js: добавили переменную Provider в которую инкапсулировали разметку StoreContext и в Props добавили props.children

--- src/StoreContext.js
+++ src/StoreContext.js
@@ -1,5 +1,13 @@
 import React from "react";
+import store from "./Redux/redux-store";
+import {BrowserRouter} from "react-router-dom";
 
 const StoreContext = React.createContext(null);
+
+export const Provider = (props) => {
+    return <StoreContext.Provider value={props.store}>
+        {props.children}
+    </StoreContext.Provider>
+}
 
 export default StoreContext;
