6591a6ad826202ae2b6afacab7f49952293aed5e
mytrofanov <mytrofanovmaxym@gmail.com>
2021-04-17 17:47:55 +0300

    47й урок. Делаем копию State для того, чтобы перерисовавалось все приложение. Все изменения пушим в копию State.

--- src/index.js
+++ src/index.js
@@ -8,22 +8,13 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
-
-let rerenderEntireTree = () => {
     ReactDOM.render(
         <BrowserRouter>
             <Provider store={store}>
             <App />
             </Provider>
         </BrowserRouter>, document.getElementById('root'));
-}
-rerenderEntireTree();
 
-store.subscribe( ()=> {
-    rerenderEntireTree();
-});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
