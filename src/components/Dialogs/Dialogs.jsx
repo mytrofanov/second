1ec353cd2710a54bd2cc284ce45ab9f558f57f4e
mytrofanov <mytrofanovmaxym@gmail.com>
2021-04-18 11:07:14 +0300

    48 передалали страницу Dialogs используюя копию state, сделали копию состояния я в DialogReducer

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -23,8 +23,8 @@ const Message = (props) => {
 const Dialogs = (props) => {
     let state = props.dialogsPage;
 
-    let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id} avatar={d.ava}/>)
-    let messagesElements = state.messages.map(m => <Message message={m.message}/>)
+    let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} key={d.id}  id={d.id} avatar={d.ava}/>)
+    let messagesElements = state.messages.map(m => <Message message={m.message} key={m.id} />)
 
     let newMessageBody = state.newMessageBody;
     let onSendMessageClick = () => {
