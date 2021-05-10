c183b3a43c9ed70dda663b0e0a7b6f986e3e652b
mytrofanov <mytrofanovmaxym@gmail.com>
2021-05-10 17:33:47 +0300

    51 API, axios установили библиотеку Axios и получили список users от сервера

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,22 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from "axios";
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'http://www.youloveit.ru/uploads/posts/2020-04/1586360515_youloveit_ru_dipper_gravity_falls_na_avu02.jpg',
-                    followed: true,
-                    fullName: 'Dmitriy',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'http://www.youloveit.ru/uploads/posts/2020-04/1586360515_youloveit_ru_dipper_gravity_falls_na_avu02.jpg',
-                    followed: false,
-                    fullName: 'Max',
-                    status: 'I am cool',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'http://www.youloveit.ru/uploads/posts/2020-04/1586360515_youloveit_ru_dipper_gravity_falls_na_avu02.jpg',
-                    followed: true,
-                    fullName: 'Serg',
-                    status: 'I am a good',
-                    location: {city: 'New York', country: 'USA'}
-                }
-            ]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        });
+
 
     }
-
-
-
     return <div>
         {
             props.users.map(u => <div key={u.id}>
         <span>
             <div>
-                <img src={u.photoUrl} className={styles.usersPhoto}/>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usersPhoto}/>
             </div>
             <div>
                 {u.followed
@@ -54,13 +30,13 @@ let Users = (props) => {
         </span>
                 <span>
             <span>
-                <div>{u.fullName}</div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
             </span>
 
             <span>
-                 <div>{u.location.country}</div>
-                 <div>{u.location.city}</div>
+                 <div>{"u.location.country"}</div>
+                 <div>{"u.location.city"}</div>
             </span>
         </span>
             </div>)
