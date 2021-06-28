6c96742b047a261fa37a1daf022940c101339bf8
mytrofanov <mytrofanovmaxym@gmail.com>
2021-06-28 16:31:10 +0300

    75 сделал валидацию полей формы login

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -4,8 +4,10 @@ import s from './login.module.css';
 import {authAPI} from "../../API/api";
 
 
+
 export default function Login() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit,
+        formState: { errors }} = useForm();
     const onSubmit = (data)  => authAPI.login(data);
 
     return (
@@ -13,10 +15,20 @@ export default function Login() {
             <h1>Login Form</h1>
         <form  onSubmit={handleSubmit(onSubmit)}>
             <div>
-            <input {...register("email")} placeholder={" e-mail"} />
+            <input {...register("email", { required: true,
+                maxLength: 40 })} placeholder={" e-mail"} />
+                {errors?.email?.type === "required" && <p>This field is required</p>}
+                {errors?.email?.type === "maxLength" && (
+                    <p>This field cannot exceed 40 characters</p>
+                )}
             </div>
             <div>
-            <input {...register("password")} placeholder={" password"}/>
+            <input {...register("password", { required: true,maxLength : 30}
+            )} placeholder={" password"}/>
+                {errors?.password?.type === "required" && <p>This field is required</p>}
+                {errors?.password?.type === "maxLength" && (
+                    <p>This field cannot exceed 30 characters</p>
+                )}
             </div>
             <div>
             <input {...register("rememberMe")} type="checkbox" />
