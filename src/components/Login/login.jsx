dd048a1ef8a9562bbd088c3db1027960618ff1e5
mytrofanov <mytrofanovmaxym@gmail.com>
2021-06-28 12:08:06 +0300

    75 вставил форму логинизации из библиотеки https://react-hook-form.com/get-started  Форма которую показывает Димыч устарела. Данные собираю в console.log

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import s from './login.module.css';
 
-const Login =(props) => {
-    return <h1>Login</h1>
+
+export default function Login() {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
+    return (
+        <div className={s.loginForm}>
+            <h1>Login Form</h1>
+        <form  onSubmit={handleSubmit(onSubmit)}>
+            <div>
+            <input {...register("login")} placeholder={" login"} />
+            </div>
+            <div>
+            <input {...register("password")} placeholder={" password"}/>
+            </div>
+            <div>
+            <input {...register("checkbox")} type="checkbox" />
+                Remember me
+            </div>
+            <input type="submit" value="Login" />
+        </form>
+        </div>
+    );
 }
 
-export default Login;
+
+
