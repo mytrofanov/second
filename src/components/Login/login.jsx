8cbeded3b198a3df37a79e15d222ddc386430bf2
mytrofanov <mytrofanovmaxym@gmail.com>
2021-06-29 09:46:01 +0300

    75 разделил логику и отрисовку формы login

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -4,40 +4,44 @@ import s from './login.module.css';
 import {authAPI} from "../../API/api";
 
 
-
-
-export default function Login() {
+export const LoginForm = () =>{
+    const onSubmit = (data)  => authAPI.login(data);
     const { register, handleSubmit,
         formState: { errors }} = useForm();
-    const onSubmit = (data)  => authAPI.login(data);
-
     return (
-        <div >
-
+        <div>
         <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
             <h1>Login Form</h1>
             <div>
-            <input {...register("email", { required: true,
-                maxLength: 40 })} placeholder={" e-mail"} />
+                <input {...register("email", { required: true,
+                    maxLength: 40 })} placeholder={" e-mail"} />
                 {errors?.email?.type === "required" && <span>This field is required</span>}
                 {errors?.email?.type === "maxLength" && (
                     <span>This field cannot exceed 40 characters</span>
                 )}
             </div>
             <div>
-            <input {...register("password", { required: true,maxLength : 30}
-            )} placeholder={" password"}/>
+                <input {...register("password", { required: true,maxLength : 30}
+                )} placeholder={" password"}/>
                 {errors?.password?.type === "required" && <span>This field is required</span>}
                 {errors?.password?.type === "maxLength" && (
                     <span>This field cannot exceed 30 characters</span>
                 )}
             </div>
             <div>
-            <input {...register("rememberMe")} type="checkbox" />
+                <input {...register("rememberMe")} type="checkbox" />
                 Remember me
             </div>
             <input type="submit" value="Login" />
         </form>
+        </div>
+    )
+}
+
+export default function Login() {
+    return (
+        <div >
+           <LoginForm/>
         </div>
     );
 }
