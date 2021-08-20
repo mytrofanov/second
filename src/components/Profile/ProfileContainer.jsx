a4bd39bee766eae6a270de341fa1e7d1c3a4e0de
mytrofanov <mytrofanovmaxym@gmail.com>
2021-08-20 16:12:01 +0300

    96 componentDidUpdate позволяет отслеживать изменения в профайле и обновляет страницу если меняются юзеры

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -9,16 +9,27 @@ import {compose} from "redux";
 
 class ProfileContainer extends React.Component {
 
-    componentDidMount() {
+    refreshProfile() {
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = this.props.authorizedUserId;
-            if(!userId) {
+            if (!userId) {
                 this.props.history.push("/login");
             }
         }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
+    }
+
+    componentDidMount() {
+        this.refreshProfile();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if ( this.props.match.params.userId !=  prevProps.match.params.userId)
+        {
+            this.refreshProfile();
+        }//если юзер меняется то обновляем компоненту profile
     }
 
     render() {
@@ -35,7 +46,7 @@ let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
     status: state.profilePage.status,
     authorizedUserId: state.auth.userId,
-    isAuth:state.auth.isAuth
+    isAuth: state.auth.isAuth
 
 });
 
