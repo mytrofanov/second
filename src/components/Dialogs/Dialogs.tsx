f6010dcaa08bf2ead473f18299d6f8c6b5ee42d4
mytrofanov <mytrofanovmaxym@gmail.com>
2021-09-15 10:00:58 +0300

    2.5 Мигрировал Dialogs.jsx на TypeScript

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,9 +2,40 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 import {useForm} from "react-hook-form";
+import dialogsReducer from "../../Redux/dialogs-reducer";
+
+type DialogItemPropsType = {
+    id: number
+    avatar?: string
+    name: string
+}
+type MessagePropsType = {
+    message: string
+}
+type MessagesType = {
+    id: number
+    message: string
+}
+type  MessagesPropsType = {
+    messages: Array<MessagesType>
+}
+
+type DialogsType = {
+    dialogs: Array<DialogItemPropsType>
+}
+
+type DialogsPageType = {
+    dialogsPage: DialogsType & MessagesPropsType
+}
+
+type SendMessageType = {
+    sendMessage: (values:string) => any
+}
+
+type DialogsPropsType = DialogsPageType & DialogsType & MessagesPropsType & SendMessageType
 
 
-const DialogItem = (props) => {
+const DialogItem = (props: DialogItemPropsType) => {
     let path = "/dialogs/" + props.id;
 
     return (
@@ -15,20 +46,20 @@ const DialogItem = (props) => {
     )
 }
 
-const Message = (props) => {
+const Message = (props: MessagePropsType) => {
     return (
         <div className={s.message}>{props.message}</div>
     )
 }
 
-const Dialogs = (props) => {
+const Dialogs = (props: DialogsPropsType) => {
     let state = props.dialogsPage;
 
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} key={d.id} id={d.id} avatar={d.ava}/>)
     let messagesElements = state.messages.map(m => <Message message={m.message} key={m.id}/>)
 
 
-    let addNewMessage = (values) => {
+    let addNewMessage = (values:any) => {
         console.log(values.newMessageBody)
         props.sendMessage(values.newMessageBody);
     }
@@ -63,8 +94,9 @@ const Dialogs = (props) => {
 export default Dialogs;
 
 
-export const AddMessageForm = (props) => {
+export const AddMessageForm = (props:any) => {
     const {register, handleSubmit, formState: {errors}} = useForm();
+    // @ts-ignore
     const onSubmit = data => props.onSubmit(data);
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
