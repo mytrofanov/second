784b738e13ed35bc0b89a19450e96002dab2c6f5
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-16 16:07:51 +0300

    поставил другие зависимости mateial ui: npm install @emotion/react npm install @emotion/styled npm i @mui/icons-material npm i @material-ui/core @material-ui/icons или yarn add @material-ui/core @material-ui/icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import s from './Header.module.css';
 import {NavLink} from "react-router-dom";
 import logo from "../../assets/images/Screenshot_1.jpg";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 type HeaderPropsType = {
     isAuth: boolean
@@ -11,6 +19,29 @@ type HeaderPropsType = {
 
 const Header: React.FC<HeaderPropsType> = (props) => {
        return (
+           <div>
+               <Box sx={{ flexGrow: 1 }}>
+                   <AppBar position="static">
+                       <Toolbar>
+                           <IconButton
+                               size="large"
+                               edge="start"
+                               color="inherit"
+                               aria-label="menu"
+                               sx={{ mr: 2 }}
+                           >
+                               <MenuIcon />
+                           </IconButton>
+                           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                               Social Network
+                           </Typography>
+                           <Button color="inherit">Login</Button>
+                       </Toolbar>
+                   </AppBar>
+               </Box>
+
+
+
         <header className={s.header}>
             <img src={logo} className={s.logo} alt='Логотип'/>
             <div className={s.loginBlock}>
@@ -23,7 +54,9 @@ const Header: React.FC<HeaderPropsType> = (props) => {
                         <NavLink to={'/login'}> Login </NavLink>}
 
             </div>
-        </header>)
+        </header>
+           </div>
+       )
 }
 
 
